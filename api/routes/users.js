572b87788143a6ed934436caa1d9696de0df8357
Gabriel da Silva <115048603+bielkh@users.noreply.github.com>
2023-03-26 23:22:49 -0300

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,13 @@
+///importando o express
 import express from "express";
+///importando o controlller de user
 import { getUsers } from "../controllers/user.js";
 
+///const router para usar como rota
 const router = express.Router();
 
+///rota com getUsers
 router.get("/", getUsers);   
 
-
+///exportando a rota
 export  default router;
